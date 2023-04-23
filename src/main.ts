9ae582fa069ac8e34d8e2aa23ba3f6caaa389605
Szymon Bartanowicz <szymon@bartanowicz.pl>
2023-04-23 22:31:45 +0200

    init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {createPinia, Pinia} from 'pinia'
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(router).use(vuetify).use(createPinia()).mount('#app')
