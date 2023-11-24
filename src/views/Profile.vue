452f7232e513023a02668c9543f213570b4b7f08
Szymon Bartanowicz <szymon@bartanowicz.pl>
2023-11-24 15:32:31 +0100

    impl

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,15 @@
 <template>
     <v-container>
-        profile
+        <v-list class="text-left">
+            <v-list-item><span>Nazwa użytkownika: </span><span class="font-weight-bold">{{ authorization.currentUser.username }}</span></v-list-item>
+            <v-list-item><span>Email: </span><span class="font-weight-bold">{{ authorization.currentUser.email }}</span></v-list-item>
+            <v-list-item>Punkty dynamiczne:</v-list-item>
+        </v-list>
     </v-container>
 </template>
 
 <script lang="ts" setup>
+import {useAuthorizationStore} from "../store/AuthorizationStore";
+
+const authorization = useAuthorizationStore()
 </script>
