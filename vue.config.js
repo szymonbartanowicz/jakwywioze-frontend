ac03d6714464d680d316eae0bac9400537abde9f
happy531 <jedrzejwesolowski177@gmail.com>
2023-12-07 13:47:00 +0100

    [HOTFIX] Fix Vue error with establishing websocket connection

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     devServer: {
+        client: {
+            webSocketURL: process.env.VUE_APP_WEBSOCKET_URL || 'localhost:3000',
+        },
         allowedHosts: [
             'jakwywioze.pl',
             'www.jakwywioze.pl'
